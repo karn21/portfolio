92a0e8025638afb101cc1b482e9a02ea689fee60
karn21 <karn212000@gmail.com>
2020-04-06 16:58:24 +0530

    Updates

---
+++ precache-manifest.b67cbf0f5b586f2f395f69e9e7faf9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72df44c735ea168e9b21c8ef0ebb693b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5633e56660a0007a05a2",
+    "url": "/static/css/main.8cbb93d7.chunk.css"
+  },
+  {
+    "revision": "1e4e0ae92aa5e9b64c23",
+    "url": "/static/js/2.d2468a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d2468a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5633e56660a0007a05a2",
+    "url": "/static/js/main.85fb93a6.chunk.js"
+  },
+  {
+    "revision": "4c15ceb5499f6abee85b",
+    "url": "/static/js/runtime-main.eb07c764.js"
+  }
+]);
