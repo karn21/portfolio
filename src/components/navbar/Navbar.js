8b8d737fc43c55d921c318629e9e3915e544b0d6
karn21 <karn212000@gmail.com>
2020-04-07 12:50:50 +0530

    fixed navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,10 +2,16 @@ import "./navbar.scss";
 import React, { Component } from "react";
 
 export class Navbar extends Component {
+  state = {
+    showMenu: false,
+  };
   showNav = () => {
+    // this.setState({
+    //   showMenu: !this.state.showMenu,
+    // });
     var menu = document.getElementById("menu");
     if (menu.style.display === "none") {
-      menu.style.display = "block";
+      menu.style.display = "grid";
     } else {
       menu.style.display = "none";
     }
@@ -14,19 +20,42 @@ export class Navbar extends Component {
   render() {
     return (
       <nav className="navbar">
-        <a
-          href="https://github.com/karn21"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="github-btn">
-            <i className="fab fa-github github-icon"></i>
-            <span className="github-text">Github Profile</span>
-          </button>
-        </a>
+        <div>
+          <a
+            href="https://github.com/karn21"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="github-btn">
+              <i className="fab fa-github github-icon"></i>
+              <span className="github-text">Github Profile</span>
+            </button>
+          </a>
+        </div>
+
+        {/* {this.state.showMenu ? (
+          <ul className="nav-links">
+            <div id="menu">
+              <a href="#">
+                <li className="nav-link">Education</li>
+              </a>
+              <a href="#">
+                <li className="nav-link">Skills</li>
+              </a>
+              <a href="#">
+                <li className="nav-link">My Works</li>
+              </a>
+              <a href="#">
+                <li className="nav-link">Contact</li>
+              </a>
+            </div>
+          </ul>
+        ) : (
+          <ul></ul>
+        )} */}
 
         <ul className="nav-links">
-          <div id="menu">
+          <div id="menu" style={{ display: "none" }}>
             <a href="#">
               <li className="nav-link">Education</li>
             </a>
