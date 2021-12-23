aa2658ade9b5261e0adc8d2c520c551929eaf2d3
karn21 <karn212000@gmail.com>
2021-12-23 19:14:56 +0530

    fix timeline order

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -4,24 +4,24 @@ import Events from "./TimelineData";
 import TimelineItem from "./TimelineItem";
 
 export class Timeline extends Component {
-  state = {
-    events: Events,
-  };
-  render() {
-    return (
-      <section id="timeline">
-        <div className="section-head">
-          <h2>My Road to Glory!</h2>
-          <hr />
-        </div>
-        <div className="timeline">
-          {this.state.events.reverse().map((event) => (
-            <TimelineItem event={event} key={event.id}></TimelineItem>
-          ))}
-        </div>
-      </section>
-    );
-  }
+	state = {
+		events: Events,
+	};
+	render() {
+		return (
+			<section id="timeline">
+				<div className="section-head">
+					<h2>My Road to Glory!</h2>
+					<hr />
+				</div>
+				<div className="timeline">
+					{this.state.events.map((event) => (
+						<TimelineItem event={event} key={event.id}></TimelineItem>
+					))}
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Timeline;
